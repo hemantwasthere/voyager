ec5f73141ff36220d57d624ba97bcf5431a91f32
Hemant <hrsmart2150@gmail.com>
2024-05-31 16:54:09 +0530

    tweaks

--- src/components/TransactionsTable/Cells/StatusCell.tsx
+++ src/components/TransactionsTable/Cells/StatusCell.tsx
@@ -3,17 +3,17 @@ import { Icons } from "../../Icons";
 import { TransactionsCellActionProps } from "../columns";
 
 export const StatusCell: React.FC<TransactionsCellActionProps> = ({ data }) => {
-  return (
-    <div>
-      {data.status === "ACCEPTED_ON_L2" ? (
-        <CustomTooltip tooltipValue="Accepted on L2">
-          <Icons.Accepted_On_L2 />
-        </CustomTooltip>
-      ) : (
-        <CustomTooltip tooltipValue="Reverted">
-          <Icons.Reverted />
-        </CustomTooltip>
-      )}
-    </div>
+  return data.status === "ACCEPTED_ON_L2" ? (
+    <CustomTooltip tooltipValue="Accepted on L2">
+      <div className="w-fit cursor-pointer">
+        <Icons.Accepted_On_L2 />
+      </div>
+    </CustomTooltip>
+  ) : (
+    <CustomTooltip tooltipValue="Reverted">
+      <div className="w-fit cursor-pointer">
+        <Icons.Reverted />
+      </div>
+    </CustomTooltip>
   );
 };
